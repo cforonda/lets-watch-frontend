cdc33ec1f4d46db3adf4f935e0b0020445ea7773
Stephen White <SteveWhite.dev@gmail.com>
2020-07-23 14:52:48 -0700

    initializing react project

---
+++ app/src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../assets/App/App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>This is a React App</h1>
+    </div>
+  );
+}
+
+export default App;
