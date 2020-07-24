06c9e55f9b0e8a81b5e1f1321e08ed62fe435f7d
Stephen White <SteveWhite.dev@gmail.com>
2020-07-23 17:08:37 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lets-watch/precache-manifest.467139c484188513e57ef2bfbe1816a7.js"
+  "/lets-watch/precache-manifest.8b4b7957c147c656e33a33ce9dc10a00.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lets-watch/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
