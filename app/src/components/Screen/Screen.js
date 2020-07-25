a5167f450662ce9cf91ebbad18524820a30e6655
Stephen White <SteveWhite.dev@gmail.com>
2020-07-25 16:30:47 -0700

    added .env.development.local with REACT_APP_API_ENDPOINT='http://localhost:8000/'

--- app/src/components/Screen/Screen.js
+++ app/src/components/Screen/Screen.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
 import YTPlayer from "../YTPlayer/YTPlayer";
+const dotenv = require('dotenv').config();
+
 
 export default function Screen( { routerProps }) {
 
     const [response, setResponse] = useState("");
 
-    const API_ENDPOINT = "https://lets-watch-backend.herokuapp.com/";
+    const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT || "https://lets-watch-backend.herokuapp.com/";
 
     console.log(API_ENDPOINT);
 
