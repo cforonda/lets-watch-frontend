8ee6d85f6a74ce511ef3ad8cb7119cdd05eb24e8
cforonda <champ.foronda@gmail.com>
2020-07-26 00:47:14 -0700

    Added a text field to add video - doesn't work correctly

--- app/src/components/Screen/Screen.js
+++ app/src/components/Screen/Screen.js
@@ -1,16 +1,21 @@
-import React, { useState, useEffect } from "react";
-import socketIOClient from "socket.io-client";
-import YTPlayer from "../YTPlayer/YTPlayer";
-import Typography from "@material-ui/core/Typography";
+import React, { useState, useEffect } from 'react';
+import socketIOClient from 'socket.io-client';
+import YTPlayer from '../YTPlayer/YTPlayer';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import '../../assets/Screen/Screen.css';
 
 const dotenv = require('dotenv').config();
-
 
 export default function Screen( { routerProps }) {
     const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT || "https://lets-watch-backend.herokuapp.com/";
 
     const [numClients, setNumClients] = useState();
+    const [videoId, setVideoId] = useState("");
+
+    const updateVideoId = text => setVideoId(text);
+    const getVideoId = (event) => updateVideoId(event.target.value);
 
     console.log(API_ENDPOINT);
 
@@ -33,8 +38,18 @@ export default function Screen( { routerProps }) {
             {numClients ? <Typography variant="h6">{numClients} users are currently watching</Typography> 
             :
             <Typography variant="h6">1 user is currently watching</Typography>}
-            <br /><br />
-            <YTPlayer id="Qll7IHN0I4Q"/>
+            <br />
+
+            <TextField id="standard-basic" type="text" value={videoId} label="Youtube Video Id" onChange={getVideoId} required />
+            
+            <br />
+
+            <br />
+            {
+                videoId ? <YTPlayer id={videoId} />
+                :
+                null
+            }
         </div>
     )
 }
